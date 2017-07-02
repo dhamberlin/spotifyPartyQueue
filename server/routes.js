aa46256ec00f98e6338e42027d3f3a5e1ff74724
David H <davidhamberlin@gmail.com>
2017-07-02 02:40:01 -0400

    stringify res

--- server/routes.js
+++ server/routes.js
@@ -8,7 +8,7 @@ routes.get('/', (req, res) => res.send('<a href="/auth">Connect with Spotify</a>
 
 routes.get('/playlist', (req, res) => {
   Spotify.getPlaylist()
-  .then(trackNames => res.send(trackNames));
+  .then(trackNames => res.send(JSON.stringify(trackNames)));
 })
 
 ///////////////////
